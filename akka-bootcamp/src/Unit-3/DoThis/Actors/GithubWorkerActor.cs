a20b41ca077260b219752b3ab285fc0e1a8c4d27
Patrick GA <gapfulgence@gmail.com>
2016-08-18 17:24:18 -0400

    Finish lesson 3.4

--- akka-bootcamp/src/Unit-3/DoThis/Actors/GithubWorkerActor.cs
+++ akka-bootcamp/src/Unit-3/DoThis/Actors/GithubWorkerActor.cs
@@ -72,20 +72,15 @@ namespace GithubActors.Actors
             {
                 // ReSharper disable once PossibleNullReferenceException (we know from the previous IS statement that this is not null)
                 var starrer = (query.Query as QueryStarrer).Login;
-                try
-                {
-                    var getStarrer = _gitHubClient.Activity.Starring.GetAllForUser(starrer);
+                var sender = Sender;
 
-                    //ewww
-                    getStarrer.Wait();
-                    var starredRepos = getStarrer.Result;
-                    Sender.Tell(new StarredReposForUser(starrer, starredRepos));
-                }
-                catch (Exception ex)
-                {
-                    //operation failed - let the parent know
-                    Sender.Tell(query.NextTry());
-                }
+                var getStarrer = _gitHubClient.Activity.Starring.GetAllForUser(starrer)
+                    .ContinueWith<object>(tr =>
+                    {
+                        if (tr.IsFaulted || tr.IsCanceled) return query.NextTry();
+
+                        return new StarredReposForUser(starrer, tr.Result);
+                    }).PipeTo(sender);
             });
 
             //query all starrers for a repository
@@ -93,20 +88,14 @@ namespace GithubActors.Actors
             {
                 // ReSharper disable once PossibleNullReferenceException (we know from the previous IS statement that this is not null)
                 var starrers = (query.Query as QueryStarrers).Key;
-                try
-                {
-                    var getStars = _gitHubClient.Activity.Starring.GetAllStargazers(starrers.Owner, starrers.Repo);
+                var sender = Sender;
+                _gitHubClient.Activity.Starring.GetAllStargazers(starrers.Owner, starrers.Repo)
+                    .ContinueWith<object>(tr =>
+                    {
+                        if (tr.IsFaulted || tr.IsCanceled) return query.NextTry();
 
-                    //ewww
-                    getStars.Wait();
-                    var stars = getStars.Result;
-                    Sender.Tell(stars.ToArray());
-                }
-                catch (Exception ex)
-                {
-                    //operation failed - let the parent know
-                    Sender.Tell(query.NextTry());
-                }
+                        return tr.Result.ToArray();
+                    }).PipeTo(sender);
             });
         }
     }
